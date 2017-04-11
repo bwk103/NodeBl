652f7b55e998b99ecfce23bbeee3cbddb8dd4ff6
Ben Kielty <bwk103@gmail.com>
2017-04-11 11:34:16 +0100

    Create Bowling.score function
    Created bowling.score function which returns the new attribute,
    bowling._score.  This keeps a cumulative tally of the pins knocked down
    by the user using the .roll function.
    
    Added two further tests for return of integer when ordinary characters
    passed in (i.e. not strikes or half strikes).

--- app/bowling.js
+++ app/bowling.js
@@ -1,10 +1,13 @@
-function Game(){
+var Game = function(){
   this._score = 0;
-}
+};
 
 Game.prototype.roll = function(num){
   this._score += num;
 };
 
+Game.prototype.score = function(){
+  return this._score;
+};
 
 exports.Game = Game;
--- test/bowlingTest.js
+++ test/bowlingTest.js
@@ -1,12 +1,31 @@
 var expect = require("chai").expect;
 var Bowling = require("../app/bowling");
 
-describe("it scored a bowling game", function(){
-  it("scores a gutter game as 0", function(){
-    var game = new Bowling.Game();
-    for (var i = 0; i < 21; i++){
-      game.roll(0);
-    }
-    expect(game._score).to.equal(0);
+describe("bowling game", function(){
+  describe("returns an integer when given a bowling score without special characters", function(){
+
+    it("scores a gutter game as 0", function(){
+      var game = new Bowling.Game();
+      for (var i = 0; i < 20; i++){
+        game.roll(0);
+      }
+      expect(game._score).to.equal(0);
+    });
+
+    it("scores a game of all 1s as 20", function(){
+      var game = new Bowling.Game();
+        for (var i = 0; i < 20; i++){
+          game.roll(1);
+        }
+      expect(game.score()).to.equal(20);
+    });
+
+    it("scores a game of all 4s as 80", function(){
+      var game = new Bowling.Game();
+      for (var i = 0; i< 20; i++){
+        game.roll(4);
+      }
+      expect(game.score()).to.equal(80);
+    });
   });
 });
